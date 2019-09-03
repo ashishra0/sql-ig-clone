64ab8adede68935df9fddae07c112c409a2149ac
Ashish Rao <ashishrao2598@gmail.com>
2019-09-03 11:09:43 +0530

    add database

--- ig_clone.sql
+++ ig_clone.sql
@@ -1,11 +1,2 @@
-DROP DATABASE ig_clone;
 CREATE DATABASE ig_clone;
 USE ig_clone;
-
-CREATE TABLE users (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
-);
-
-INSERT INTO users (username) VALUES ('officialar8'), ('mnikita97');
