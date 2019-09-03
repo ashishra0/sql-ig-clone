7604eabda5991641b50ef8411305939b2c76eed1
Ashish Rao <ashishrao2598@gmail.com>
2019-09-03 13:40:10 +0530

    create like table

---
+++ likes/likes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE likes (
+  user_id INTEGER NOT NULL,
+  photo_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(photo_id) REFERENCES photos(id),
+  PRIMARY KEY(user_id, photo_id)
+);
