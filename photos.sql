2da0118e0f1e758f7f0da5d1380e698431ef9a3f
Ashish Rao <ashishrao2598@gmail.com>
2019-09-03 11:26:48 +0530

    add photos table

--- photos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE photos (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  image_url VARCHAR(255) NOT NULL,
-  user_id INTEGER NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  FOREIGN KEY(user_id) REFERENCES users(id)
-);
