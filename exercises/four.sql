3d5583016be5531fb53ac1f0b6a610cf6e36919c
Ashish Rao <ashishrao2598@gmail.com>
2019-09-04 22:42:27 +0530

    add comment

--- exercises/four.sql
+++ exercises/four.sql
@@ -1,1 +1,3 @@
+-- Figure out which posts has the most likes
+
 SELECT users.username, photos.id, photos.image_url, COUNT(photos.user_id) AS total FROM photos JOIN likes ON likes.photo_id = photos.id JOIN users ON photos.user_id = users.id GROUP BY photos.id ORDER BY total DESC LIMIT 1;
