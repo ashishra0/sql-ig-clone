d7a92b944ea907c70e925d6f04af83c2b1124950
Ashish Rao <ashishrao2598@gmail.com>
2019-09-04 22:21:59 +0530

    add exercise no 4

---
+++ exercises/four.sql
@@ -0,0 +1,1 @@
+SELECT users.username, photos.id, photos.image_url, COUNT(photos.user_id) AS total FROM photos JOIN likes ON likes.photo_id = photos.id JOIN users ON photos.user_id = users.id GROUP BY photos.id ORDER BY total DESC LIMIT 1;
